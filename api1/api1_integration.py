b33f7dffadd85c37eb0e65199fa7dc54d42ed1f1
Jeremy Rich <jeremy.rich@berjak.com.au>
2024-07-12 19:07:52 +1000

    Set up project, integrated APIs, and created main script

---
+++ api1/api1_integration.py
@@ -0,0 +1,14 @@
+import os
+import requests
+
+def get_data_from_api1():
+    api_key = os.getenv('API_KEY')
+    response = requests.get('ACTUAL_API_1_ENDPOINT', headers={'Authorization': f'Bearer {api_key}'})
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
+
+if __name__ == "__main__":
+    data = get_data_from_api1()
+    print(data)
